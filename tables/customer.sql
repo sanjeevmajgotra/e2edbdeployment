ed8d0bfd731050d2677c8512e62ad95a02a6dbba
majgotra <majgotra@yahoo.com>
2020-08-01 22:40:39 -0400

    Add project files.

---
+++ tables/customer.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[customer]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [firstname] VARCHAR(50) NOT NULL, 
+    [lastname] VARCHAR(50) NOT NULL, 
+    [dateofbirth] DATE NOT NULL
+)
